d24a95bf56fa1e7e3aff0704a09d88e363bc4c25
Fabioyt <fabioott123@gmail.com>
2024-06-09 20:56:47 +0200

    Autocommit

--- src/app/services/garage.service.ts
+++ src/app/services/garage.service.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { Observable, combineLatest } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { fromUserCoordinate } from 'ol/proj';
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +16,19 @@ export class GarageService {
     console.log("friendUser", userId);
     return this.afs.collection(`users/${userId}/data/data/friends`).snapshotChanges().pipe(
       switchMap(friends => {
-        const friendIds = friends.map(friend => friend.payload.doc.id);
-        const friendGarages = friendIds.map(friendId =>
-          this.afs.collection(`users/${friendId}/data/data/garages`).valueChanges()
-        );
+        // const friendIds = friends.map(friend => friend.payload.doc.id);
+        // const friendGarages = friendIds.map(friendId =>
+        //   this.afs.collection(`users/${friendId}/data/data`).valueChanges()
+        // );
+        let friendGarages: any[] = []
+        this.authService.getDataFromUser(userId).subscribe((e:any) => {
+          console.log(e.garages);
+          friendGarages.push(...e.garages)
+        })
+
         console.log("friendGarage", friendGarages);
         
-        return combineLatest(friendGarages);
+        return (friendGarages);
       }),
       map(garagesArray => garagesArray.flat())
     );
